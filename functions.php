54b4983784489ca59b62e4855c4b6e18e81a2d9a
Ka Hei Lam <lkhahaha@gmail.com>
2015-03-13 21:38:47 +0800

    featured img

--- functions.php
+++ functions.php
@@ -38,7 +38,7 @@ function hellonote_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -163,4 +163,109 @@ echo '<script type="text/javascript">
   //]]>
   </script>';
 }
-add_action('admin_print_footer_scripts', 'my_print_footer_scripts', 21);
+add_action( 'admin_print_footer_scripts' , 'my_print_footer_scripts' , 21 );
+
+/* set a featured image with external image URL */
+// for admin ui
+add_filter( 'admin_post_thumbnail_html', 'thumbnail_url_field' );
+
+// for saving thumbnail url
+add_action( 'save_post', 'thumbnail_url_field_save', 10, 2 );
+
+// for displaying
+add_filter( 'post_thumbnail_html', 'thumbnail_external_replace', 10, PHP_INT_MAX );
+
+// 
+function url_is_image( $url ) {
+	if ( ! filter_var( $url, FILTER_VALIDATE_URL ) ) {
+		return FALSE;
+	}
+	$ext = array( 'jpeg', 'jpg', 'gif', 'png' );
+	$info = (array) pathinfo( parse_url( $url, PHP_URL_PATH ) );
+	return isset( $info['extension'] ) && in_array( strtolower( $info['extension'] ), $ext, TRUE );
+}
+
+// output textbox in admin
+function thumbnail_url_field( $html ) {
+	global $post;
+	$value = get_post_meta( $post->ID, '_thumbnail_ext_url', TRUE ) ? : "";
+	$nonce = wp_create_nonce( 'thumbnail_ext_url_' . $post->ID . get_current_blog_id() );
+	$html .= '<input type="hidden" name="thumbnail_ext_url_nonce" value="' . esc_attr( $nonce ) . '">';
+	$html .= '<div><p>' . __('Or', 'txtdomain') . '</p>';
+	$html .= '<p>' . __( 'Enter the url for external image', 'txtdomain' ) . '</p>';
+	$html .= '<p><input type="url" name="thumbnail_ext_url" value="' . $value . '"></p>';
+	if ( ! empty($value) && url_is_image( $value ) ) {
+		$html .= '<p><img style="max-width:150px;height:auto;" src="' . esc_url($value) . '"></p>';
+		$html .= '<p>' . __( 'Leave url blank to remove.', 'txtdomain' ) . '</p>';
+	}
+	$html .= '</div>';
+	return $html;
+}
+
+// save thumbnail url
+function thumbnail_url_field_save( $pid, $post ) {
+	$cap = $post->post_type === 'page' ? 'edit_page' : 'edit_post';
+	if(!current_user_can($cap, $pid) || ! post_type_supports( $post->post_type, 'thumbnail') || defined('DOING_AUTOSAVE')) {
+		return;
+	}
+	$action = 'thumbnail_ext_url_' . $pid . get_current_blog_id();
+	$nonce = filter_input( INPUT_POST, 'thumbnail_ext_url_nonce', FILTER_SANITIZE_STRING );
+	$url = filter_input( INPUT_POST,  'thumbnail_ext_url', FILTER_VALIDATE_URL );
+	if(empty($nonce) || ! wp_verify_nonce($nonce, $action) || (!empty($url) && ! url_is_image( $url ))){
+		return;
+	}
+	if ( ! empty( $url ) ) {
+		update_post_meta( $pid, '_thumbnail_ext_url', esc_url($url) );
+		if ( ! get_post_meta( $pid, '_thumbnail_id', TRUE ) ) {
+	  		update_post_meta( $pid, '_thumbnail_id', 'by_url' );
+		}
+	} elseif ( get_post_meta( $pid, '_thumbnail_ext_url', TRUE ) ) {
+		delete_post_meta( $pid, '_thumbnail_ext_url' );
+		if ( get_post_meta( $pid, '_thumbnail_id', TRUE ) === 'by_url' ) {
+	  		delete_post_meta( $pid, '_thumbnail_id' );
+		}
+	}
+}
+
+// display the image
+function thumbnail_external_replace( $html, $post_id ) {
+	$url =  get_post_meta( $post_id, '_thumbnail_ext_url', TRUE );
+	if ( empty( $url ) || ! url_is_image( $url ) ) {
+		return $html;
+	}
+	$alt = get_post_field( 'post_title', $post_id ) . ' ' .  __( 'thumbnail', 'txtdomain' );
+	$attr = array( 'alt' => $alt );
+	$attr = apply_filters( 'wp_get_attachment_image_attributes', $attr, NULL );
+	$attr = array_map( 'esc_attr', $attr );
+	$html = sprintf( '<img src="%s"', esc_url($url) );
+	foreach ( $attr as $name => $value ) {
+		$html .= " $name=" . '"' . $value . '"';
+	}
+	$html .= ' />';
+	return $html;
+}
+/* set a featured image with external image URL */
+
+/* show featured image in admin all post page*/
+function manage_posts_columns($columns) {
+    $columns['thumbnail'] = __('Thumbnail');
+    return $columns;
+}
+function add_column($column_name, $post_id) {
+    if ( 'thumbnail' == $column_name) {
+        $thum = get_the_post_thumbnail($post_id, array(50,50), 'thumbnail');
+	    if ( isset($thum) && $thum ) {
+	        echo "<div style='max-height: 50px; max-width:50px; overflow: hidden;'>".$thum."</div>";
+	    } elseif( esc_attr(get_post_meta($post_id, 'eye', true)) ) {
+	        echo '<img src="';
+	        echo esc_attr(get_post_meta($post_id, 'eye', true));
+	        echo '" width="50" height="50" alt="Thumbnail" class="wp-post-image">';
+	    } else {
+	        echo '<span style="font-size:1.5em;color:red;font-weight:bold">';
+	        echo __('None');
+	        echo '</span>';
+	    }
+    }
+}
+add_filter( 'manage_posts_columns', 'manage_posts_columns' );
+add_action( 'manage_posts_custom_column', 'add_column', 10, 2 );
